5200e477e67736c25cc931f5f6643e5911ab4d71
ab3d2fghi1 <chaeleeshme@gmail.com>
2023-07-07 02:50:12 +0900

    콜라 문제

--- 콜라 문제.py
+++ 콜라 문제.py
@@ -2,10 +2,10 @@ import sys
 sys.setrecursionlimit(10000)
 def solution(a, b, n):
     answer=0
-    def recurision(coke):
+    def recursion(coke):
         if coke<a or coke//a==0: return
         nonlocal answer
         answer+=coke//a*b
-        recurision((coke//a)*b+coke%a)
-    recurision(n)
+        recursion((coke//a)*b+coke%a)
+    recursion(n)
     return answer
