b3d82cbf20966008e4cf45934ec8faf5966b4e67
ab3d2fghi1 <chaeleeshme@gmail.com>
2024-02-02 15:37:07 +0900

    Delete 문자열 정렬하기 (2).py

--- 문자열 정렬하기 (2).py
+++
@@ -1,9 +0,0 @@
-def solution(my_string):
-    answer=''
-    for i in my_string:
-        if i.isupper()==True: 
-            answer+=i.lower()
-        else: answer+=i
-    answer=list(answer)
-    answer.sort()
-    return ''.join(answer)
