720b9b06b884f9f4d26ed9d461b33c8dc8c80e1a
ab3d2fghi1 <chaeleeshme@gmail.com>
2024-02-02 15:34:21 +0900

    Delete A로 B 만들기.py

--- A로 B 만들기.py
+++
@@ -1,9 +0,0 @@
-def solution(before, after):
-    before=list(before)
-    before.sort()
-    after=list(after)
-    after.sort()
-    for i in range(len(before)):
-        if before[i]==after[i]: pass
-        else: return 0
-    return 1
