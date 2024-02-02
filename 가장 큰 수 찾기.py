ae11103545c85a92bf8d8799951ba29283223cb8
ab3d2fghi1 <chaeleeshme@gmail.com>
2024-02-02 15:36:40 +0900

    Delete 가장 큰 수 찾기.py

--- 가장 큰 수 찾기.py
+++
@@ -1,7 +0,0 @@
-def solution(array):
-    answer = []
-    for i in range(len(array)):
-        if array[i]==max(array):
-            answer.append(max(array))
-            answer.append(i)
-    return answer
