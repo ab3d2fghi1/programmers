a26e769617ed8c8213e056db0ba560024acd003d
ab3d2fghi1 <chaeleeshme@gmail.com>
2024-02-02 15:37:27 +0900

    Delete 영어가 싫어요.py

--- 영어가 싫어요.py
+++
@@ -1,6 +0,0 @@
-def solution(numbers):
-    num_list=["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    for i in range(len(num_list)):
-        if num_list[i] in numbers: numbers=numbers.replace(num_list[i],str(i))
-    numbers=int(numbers)
-    return numbers
