4a5036d139b58ad66f3a531fb1c18bbb94387e12
ab3d2fghi1 <chaeleeshme@gmail.com>
2024-02-02 15:38:01 +0900

    Delete 최댓값 만들기 (2).py

--- 최댓값 만들기 (2).py
+++
@@ -1,6 +0,0 @@
-def solution(numbers):
-    answer=min(numbers)*max(numbers)
-    for i in range(len(numbers)-1):
-        for j in range(i+1,len(numbers)):
-            if (numbers[i]*numbers[j])>answer: answer=(numbers[i]*numbers[j])
-    return answer
